8a65cd2a4482681c1877c684d404aedcb32309c9
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-06-26 14:40:24 +0530

    Version control changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\src\flutter"
+export "FLUTTER_ROOT=D:\src\flutter" 
 export "FLUTTER_APPLICATION_PATH=D:\Projects\quizzler-flutter"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
